b641e94a34bb5ba7f7879e371f9e6cadb26d9dcf
Jonathon Black <67678754+JBlack96@users.noreply.github.com>
2023-03-29 11:38:44 +0100

    added dynamo functionality for insert, get and get all inventory items

--- inventory-service/internal/aws/dynamodb/inventory_mangement.go
+++ inventory-service/internal/aws/dynamodb/inventory_mangement.go
@@ -2,6 +2,9 @@ package dynamodb
 
 import (
 	"github.com/JBlack96/golang-microservice-shop/inventory-service/internal/domain"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
@@ -16,13 +19,59 @@ func New(db dynamodbiface.DynamoDBAPI) *InventoryManagementService {
 }
 
 func (ims *InventoryManagementService) GetAllItems() ([]domain.InventoryItem, error) {
+	input := &dynamodb.ScanInput{
+		TableName: aws.String("inventory"),
+	}
+
+	output, err := ims.db.Scan(input)
+	if err != nil {
+		return []domain.InventoryItem{}, err
+	}
+
+	var items []domain.InventoryItem
+	if err = dynamodbattribute.UnmarshalListOfMaps(output.Items, &items); err != nil {
+		return []domain.InventoryItem{}, err
+	}
+
 	return []domain.InventoryItem{}, nil
 }
 
 func (ims *InventoryManagementService) GetSingleItem(ID string) (domain.InventoryItem, error) {
-	return domain.InventoryItem{}, nil
+	input := &dynamodb.GetItemInput{
+		TableName: aws.String("inventory"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {S: aws.String(ID)},
+		},
+	}
+
+	output, err := ims.db.GetItem(input)
+	if err != nil {
+		return domain.InventoryItem{}, err
+	}
+
+	var item domain.InventoryItem
+	if err = dynamodbattribute.UnmarshalMap(output.Item, &item); err != nil {
+		return domain.InventoryItem{}, err
+	}
+
+	return item, nil
 }
 
 func (ims *InventoryManagementService) AddSingleItem(item domain.InventoryItem) (domain.InventoryItem, error) {
-	return domain.InventoryItem{}, nil
+	marshalledItem, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		return domain.InventoryItem{}, err
+	}
+
+	input := &dynamodb.PutItemInput{
+		TableName:           aws.String("inventory"),
+		Item:                marshalledItem,
+		ConditionExpression: aws.String("attribute_not_exists(id)"),
+	}
+
+	if _, err := ims.db.PutItem(input); err != nil {
+		return domain.InventoryItem{}, err
+	}
+
+	return item, nil
 }
