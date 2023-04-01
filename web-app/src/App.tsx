994e21a2dde2eba1723d62b9f8527c9e451f519d
Jonathon Black <67678754+JBlack96@users.noreply.github.com>
2023-04-01 09:57:02 +0100

    added basic page design for items

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="font-thin">
-      E-Commerce Shop
-    </div>
-  );
+    const items = [
+        {id: '99999', name: 'Toothbrush', price: 9.99, imgSrc: 'https://s.alicdn.com/@sc04/kf/He6ab2b71f3304b44a49ad19575a004389.jpg_280x280.jpg',},
+        {id: '999919', name: 'Toothbrush', price: 9.99, imgSrc: 'https://s.alicdn.com/@sc04/kf/He6ab2b71f3304b44a49ad19575a004389.jpg_280x280.jpg',},
+        {id: '999991', name: 'Toothbrush', price: 9.99, imgSrc: 'https://s.alicdn.com/@sc04/kf/He6ab2b71f3304b44a49ad19575a004389.jpg_280x280.jpg',}
+    ];
+
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start"></div>
+                <div className="navbar-center">
+                    <a className="btn btn-ghost normal-case text-xl">Golang-Microservice-Shop</a>
+                </div>
+                <div className="navbar-end"></div>
+            </div>
+            <div className="flex flex-wrap m-auto max-w-screen-2xl">
+                {items.map(item =>
+                    <div key={item.id} className="card basis-1/3">
+                        <figure className="px-10 pt-10">
+                            <img src={item.imgSrc} alt="Shoes" className="rounded-xl"/>
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">{item.name}</h2>
+                            <p>Price: £{item.price}</p>
+                            <div className="card-actions">
+                                <button className="btn btn-primary">View</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- web-app/tailwind.config.js
+++ web-app/tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [{
+      mytheme: {
+        "primary": "#9838d8",
+        "secondary": "#5bd13a",
+        "accent": "#e8bdf9",
+        "neutral": "#2A232F",
+        "base-100": "#EEF1F6",
+        "info": "#5084CE",
+        "success": "#2DC897",
+        "warning": "#915608",
+        "error": "#F15E4B",
+      },
+    }]
+  },
+  plugins: [require("daisyui")],
 }
 
